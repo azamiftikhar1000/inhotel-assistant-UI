57a7c2a0f28170e64e105697d384dab9310393d6
Azam Iftikhar <azamiftikhar1000@gmail.com>
2024-06-26 13:38:44 +0530

    inhotel branding

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,12 +14,12 @@ const fontSans = FontSans({
   variable: '--font-sans'
 })
 
-const title = 'Morphic'
+const title = 'Inhotel'
 const description =
-  'A fully open-source AI-powered answer engine with a generative UI.'
+  'Generative AI assistants to hospitality staff: Guest Relations (customer support), Revenue, etc. They tirelessly handle repetitive tasks, collaborate, learn, and mature in their roles. Imagine your staff, relaxed and empowered, creating incredible guest experiences that drive long-term success.'
 
 export const metadata: Metadata = {
-  metadataBase: new URL('https://morphic.sh'),
+  metadataBase: new URL('https://inhotel.io'),
   title,
   description,
   openGraph: {
